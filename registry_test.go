61ab63c188d0bb854106db411885a249d72847cd
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-23 09:08:52 +0200

    Squashed 'src/github.com/rcrowley/go-metrics/' content from commit ef0c40b
    git-subtree-dir: src/github.com/rcrowley/go-metrics
    git-subtree-split: ef0c40b7a2d641ab732c0f8e3db69e6fea60e075

---
+++ registry_test.go
@@ -0,0 +1,36 @@
+package metrics
+
+import "testing"
+
+func BenchmarkRegistry(b *testing.B) {
+	r := NewRegistry()
+	r.Register("foo", NewCounter())
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.Each(func(string, interface{}) {})
+	}
+}
+
+func TestRegistry(t *testing.T) {
+	r := NewRegistry()
+	r.Register("foo", NewCounter())
+	i := 0
+	r.Each(func(name string, iface interface{}) {
+		i++
+		if "foo" != name {
+			t.Fatal(name)
+		}
+		if _, ok := iface.(Counter); !ok {
+			t.Fatal(iface)
+		}
+	})
+	if 1 != i {
+		t.Fatal(i)
+	}
+	r.Unregister("foo")
+	i = 0
+	r.Each(func(string, interface{}) { i++ })
+	if 0 != i {
+		t.Fatal(i)
+	}
+}
