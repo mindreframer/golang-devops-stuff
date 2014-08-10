9a9703825aa12d1cad23a593afd879d9866fdef1
Roman Heinrich <roman.heinrich@gmail.com>
2014-08-10 13:47:44 +0200

    Squashed 'src/github.com/kelseyhightower/confd/' changes from fa6c096..e2a25af
    e2a25af Merge pull request #112 from carmstrong/add_dial_timeout
    ab64315 Set DialTimeout in etcd client
    7f5790f Update deps
    2a5d4a3 Merge pull request #114 from jhmartin/patch-1
    f92a4c8 Update consul-getting-started.md
    9b19fe5 Merge pull request #106 from philipz/patch-1
    e8a5dc3 Merge pull request #109 from billhathaway/sortEtcdResults
    7d77798 sort results from etcd so keys are always in the same order
    180a934 Fix myapp-nginx.tmpl filename
    4eea42f confd: exit handle signals and exit cleanly
    40b9e8a bump version 0.5.0
    978a8b0 Merge pull request #88 from jonboulle/master
    822af37 Fix some small docs/docstring typos
    bdf1076 Version bump to 0.4.2
    9dcb38a Bump consul and etcd clients
    4eeb24b Merge pull request #87 from bacongobbler/67-log-cmd
    8531990 log reload_cmd and check_cmd output
    3043b91 Merge pull request #63 from ernesto-jimenez/master
    5a276f9 Merge 'kelseyhightower/master'
    fe3e600 integration test just use localhost
    bd2e033 Don't remove temp file if not initialized
    30d6d8a Move backend selection to backends package
    f0ee4b5 Use os.FileMode when comparing file stats
    fa2fed1 Move StoreClient interface to backends package
    38f43f9 Add license header
    65652b6 Add -version flag. Print the version and exit
    4797ce2 Merge branch 'master' of github.com:kelseyhightower/confd
    658d330 Move all confd backends to the backends package
    f42e822 Merge pull request #72 from elsonrodriguez/master
    413eddc Merge pull request #77 from raizyr/master
    63371c4 Merge pull request #74 from fritzy/master
    589557b gofmt FTW
    cd99c2a added MapDir function for mapping out etcd directories
    b548ae5 create tempfiles in the destination directory to avoid cross-filesystem issues
    3a20318 Add support for consul iteration
    f238a00 Document the -backend flag
    a9865cf Change title
    c4ab8a5 Add docs for env backend
    e955a03 Add support for env backend and new -backend flag
    2260a84 Fixes #71, broken link to quickstart guide.
    9c384a3 Always trim prefix from keys
    7e1187f Add consul getting started guide
    5e61045 Better confd run example
    7123940 Better confd run example
    4a7d719 Add confd run example
    81d3d3b Use relative links
    ea6d38f Fix typo in docs
    371f5d2 Add consul and etcd getting started guides
    e41e091 Add consul usage examples
    a6a004f Refactor resource template package.
    2fa53dd Update documentation
    9101021 Support for reusing templates in various resources
    cdb1a9d Allow use of dashes in keys
    91f4cbc Fix %d should be %s according to govet
    6970fae Merge pull request #61 from rafecolton/fix-for-handling-multiple-etcd-nodes-in-config
    fc27310 Loading config bytes manually instead of with `toml.DecodeFile`
    a7c0c23 Version bump to 0.4.0
    056b600 Respect the prefix configuration option
    74ed2ac Update dependencies
    712d81f Fix broken tests
    bc9875d Update deps
    2124c4b Fix cannot use &node (type **etcd.Node) error
    d2d468c Add godep support
    72015c6 Merge pull request #54 from benschw/master
    1259346 fix typo in readme
    70b82b2 Merge pull request #53 from armon/master
    84f0ec5 Updating docs
    19b8037 Coerce values to strings instead of byte slice
    d4796db Hack with the imports to preserve
    cf74f69 Testing cleanKeys
    0152e29 Fixing the tests
    0034d10 Adding ability to use Consul client
    e1bc86f Adding basic consul client
    d41aa4f Adding Consul configuration
    c8e465f Removing prefix from etcd client
    b2af066 Move prefix processing out of client and into template resource
    a6aa6d8 EtcdClient implements StoreClient
    ab4e5e9 Switch concrete etcdClient to StoreClient interface
    d18d088 Merge pull request #50 from billhathaway/updateDocVersions
    ac98b78 Merge pull request #51 from bacongobbler/patch-1
    f8b83fb fix typo
    5084fae change version id to 0.3.0 and update download path in docs
    62be377 Merge pull request #47 from gabrtv/fix-dangling-fd
    bf9747a defer tempfile close to avoid dangling FD
    
    git-subtree-dir: src/github.com/kelseyhightower/confd
    git-subtree-split: e2a25af3a885b2b431c5f592801d632bf2a18e95

--- docs/command-line-flags.md
+++ docs/command-line-flags.md
@@ -13,6 +13,9 @@ Usage of confd:
   -client-key="": the client key
   -confdir="/etc/confd": confd conf directory
   -config-file="": the confd config file
+  -consul=false: specified to enable use of Consul
+  -consul-addr="": address of Consul HTTP interface
+  -backend="": configuration backend to use. (consul, etcd, or env)
   -debug=false: enable debug logging
   -etcd-scheme="http": the etcd URI scheme. (http or https)
   -interval=600: etcd polling interval
--- resource/template/resource_test.go
+++ resource/template/resource_test.go
@@ -7,14 +7,38 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"text/template"
 
-	"github.com/coreos/go-etcd/etcd"
 	"github.com/kelseyhightower/confd/config"
-	"github.com/kelseyhightower/confd/etcd/etcdtest"
 	"github.com/kelseyhightower/confd/log"
 )
+
+type MockKey struct {
+	Key   string
+	Value string
+}
+
+type MockStore struct {
+	Keys []*MockKey
+}
+
+func (m *MockStore) GetValues(keys []string) (map[string]interface{}, error) {
+	vals := make(map[string]interface{})
+	for _, key := range keys {
+		for _, set := range m.Keys {
+			if strings.HasPrefix(set.Key, key) {
+				vals[set.Key] = set.Value
+			}
+		}
+	}
+	return vals, nil
+}
+
+func (m *MockStore) AddKey(key string, value string) {
+	m.Keys = append(m.Keys, &MockKey{key, value})
+}
 
 // createTempDirs is a helper function which creates temporary directories
 // required by confd. createTempDirs returns the path name representing the
@@ -54,6 +78,16 @@ dest = "/does/not/exist"
 keys = [
   "/foo"
   "/bar"
+]
+`
+
+var templateResourceConfigWithPrefixTmpl = `
+[template]
+prefix = "/template_prefix"
+src = "{{ .src }}"
+dest = "{{ .dest }}"
+keys = [
+  "/foo",
 ]
 `
 
@@ -107,9 +141,79 @@ func TestProcessTemplateResources(t *testing.T) {
 	config.SetConfDir(tempConfDir)
 
 	// Create the stub etcd client.
-	c := etcdtest.NewClient()
-	fooResp := &etcd.Response{Action: "GET", Node: &etcd.Node{Key: "/foo", Dir: false, Value: "bar"}}
-	c.AddResponse("/foo", fooResp)
+	c := &MockStore{}
+	c.AddKey("/foo", "bar")
+
+	// Process the test template resource.
+	runErrors := ProcessTemplateResources(c)
+	if len(runErrors) > 0 {
+		for _, e := range runErrors {
+			t.Errorf(e.Error())
+		}
+	}
+	// Verify the results.
+	expected := "foo = bar"
+	results, err := ioutil.ReadFile(destFile.Name())
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if string(results) != expected {
+		t.Errorf("Expected contents of dest == '%s', got %s", expected, string(results))
+	}
+}
+
+func TestProcessTemplateResourcesWithPrefix(t *testing.T) {
+	log.SetQuiet(true)
+	// Setup temporary conf, config, and template directories.
+	tempConfDir, err := createTempDirs()
+	if err != nil {
+		t.Errorf("Failed to create temp dirs: %s", err.Error())
+	}
+	defer os.RemoveAll(tempConfDir)
+
+	// Create the src template.
+	srcTemplateFile := filepath.Join(tempConfDir, "templates", "foo.tmpl")
+	err = ioutil.WriteFile(srcTemplateFile, []byte("foo = {{ .foo }}"), 0644)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// Create the dest.
+	destFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Errorf("Failed to create destFile: %s", err.Error())
+	}
+	defer os.Remove(destFile.Name())
+
+	// Create the template resource configuration file.
+	templateResourcePath := filepath.Join(tempConfDir, "conf.d", "foo.toml")
+	templateResourceFile, err := os.Create(templateResourcePath)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	tmpl, err := template.New("templateResourceConfigWithPrefixTmpl").Parse(templateResourceConfigWithPrefixTmpl)
+	if err != nil {
+		t.Errorf("Unable to parse template resource template: %s", err.Error())
+	}
+	data := make(map[string]string)
+	data["src"] = "foo.tmpl"
+	data["dest"] = destFile.Name()
+	err = tmpl.Execute(templateResourceFile, data)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// Load the confd configuration settings.
+	if err := config.LoadConfig(""); err != nil {
+		t.Errorf(err.Error())
+	}
+	config.SetPrefix("")
+	// Use the temporary tempConfDir from above.
+	config.SetConfDir(tempConfDir)
+
+	// Create the stub etcd client.
+	c := &MockStore{}
+	c.AddKey("/template_prefix/foo", "bar")
 
 	// Process the test template resource.
 	runErrors := ProcessTemplateResources(c)
@@ -181,9 +285,8 @@ func TestProcessTemplateResourcesNoop(t *testing.T) {
 	config.SetNoop(true)
 
 	// Create the stub etcd client.
-	c := etcdtest.NewClient()
-	fooResp := &etcd.Response{Action: "GET", Node: &etcd.Node{Key: "/foo", Dir: false, Value: "bar"}}
-	c.AddResponse("/foo", fooResp)
+	c := &MockStore{}
+	c.AddKey("/foo", "bar")
 
 	// Process the test template resource.
 	runErrors := ProcessTemplateResources(c)
@@ -214,8 +317,9 @@ func TestBrokenTemplateResourceFile(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	// Create the stub etcd client.
-	c := etcdtest.NewClient()
+	// Create the stub client.
+	c := &MockStore{}
+
 	// Process broken template resource config file.
 	_, err = NewTemplateResourceFromPath(tempFile.Name(), c)
 	if err == nil {
@@ -295,5 +399,60 @@ func TestIsFileExist(t *testing.T) {
 	result = isFileExist(f.Name())
 	if result != true {
 		t.Errorf("Expected IsFileExist(%s) to be true, got %v", f.Name(), result)
+	}
+}
+
+type PathToKeyTest struct {
+	key, prefix, expected string
+}
+
+var pathToKeyTests = []PathToKeyTest{
+	// Without prefix
+	{"/nginx/port", "", "nginx_port"},
+	{"/nginx/worker_processes", "", "nginx_worker_processes"},
+	{"/foo/bar/mat/zoo", "", "foo_bar_mat_zoo"},
+	// With prefix
+	{"/prefix/nginx/port", "/prefix", "nginx_port"},
+	// With prefix and trailing slash
+	{"/prefix/nginx/port", "/prefix/", "nginx_port"},
+}
+
+func TestPathToKey(t *testing.T) {
+	for _, pt := range pathToKeyTests {
+		result := pathToKey(pt.key, pt.prefix)
+		if result != pt.expected {
+			t.Errorf("Expected pathToKey(%s, %s) to == %s, got %s",
+				pt.key, pt.prefix, pt.expected, result)
+		}
+	}
+}
+
+func TestCleanKeys(t *testing.T) {
+	pre := map[string]interface{}{
+		"/my/cool_val/here":     "test",
+		"/this_key":             "foo",
+		"/prefix/key":           "test",
+		"/my/new-cool-val/here": "bar",
+		"prefix/noslash":        "test",
+	}
+	config.SetPrefix("/prefix")
+	clean := cleanKeys(pre, "/prefix")
+	if len(clean) != len(pre) {
+		t.Fatalf("bad length")
+	}
+	if _, ok := clean["my_cool_val_here"]; !ok {
+		t.Fatalf("bad: %v", clean)
+	}
+	if _, ok := clean["this_key"]; !ok {
+		t.Fatalf("bad: %v", clean)
+	}
+	if _, ok := clean["key"]; !ok {
+		t.Fatalf("bad: %v", clean)
+	}
+	if _, ok := clean["my_new_cool_val_here"]; !ok {
+		t.Fatalf("bad: %v", clean)
+	}
+	if _, ok := clean["noslash"]; !ok {
+		t.Fatalf("bad: %v", clean)
 	}
 }
