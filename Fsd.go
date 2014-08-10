de7bb520e98bf7894cfa17f32cad91173a80fd6b
Roman Heinrich <roman.heinrich@gmail.com>
2014-08-10 13:46:51 +0200

    Squashed 'src/github.com/happypancake/fsd/' changes from 0679831..0c11adb
    0c11adb Don't do string manipulation if it isn't needed
    4789ba8 Adds hostname as key prefix
    49921ec Fix FSD to get first key before watching
    d222c75 Store etcd key in the component
    0236754 Added dynamic configuratoin support with etcd
    2b7f04b Log address
    3f6e409 Tweaking FSD connection logic
    f5c675a Specify custom statsd address via flag
    3fcdd55 Merge pull request #1 from abdullin/master
    e62521e Replace sprintf with string concat for speed
    bec3df5 Fixed minor things, added Histogram and changed arguments to Timer
    6f05100 Enable CountL in send
    ab74c8a Get rid of recursion in send
    478f111 Fix my typo in TimeSince
    1bfd9bf Added TimeSince functions
    d241cd2 Renamed value to milliseconds in Timer functions
    
    git-subtree-dir: src/github.com/happypancake/fsd
    git-subtree-split: 0c11adbd11c6acd92b0676c6358cde466e96f05a

--- Fsd.go
+++ Fsd.go
@@ -1,3 +1,5 @@
+// Package fsd is a client library for accessing StatsD daemon. You can specify
+// custom StatsD port via statsd flag
 package fsd
 
 import (
@@ -5,51 +7,98 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"strconv"
 	"time"
+
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/op/go-logging"
 )
 
 var (
-	Instance *Fsd
+	address string
+
+	addressConfig chan *etcd.Response
+	outgoing      = make(chan []byte, 100000)
+
+	log = logging.MustGetLogger("fsd")
+
+	conn net.Conn
 )
 
-type Fsd struct {
-	outgoing chan string
-	address  string
-	conn     net.Conn
+func InitWithDynamicConfig(client *etcd.Client, hostname string) {
+	addressConfig = make(chan *etcd.Response)
+
+	go watchConfiguration(client, fmt.Sprintf("%v/statsd/address", hostname))
+	go processOutgoing()
 }
 
-func init() {
-	Start("127.0.0.1:8125")
-}
-
-func Start(address string) {
-	Instance = &Fsd{address: address, outgoing: make(chan string, 100000)}
-	Instance.connect()
-
-	go Instance.processOutgoing()
-}
-
-func (fsd *Fsd) connect() error {
-	conn, err := net.Dial("udp", fsd.address)
-	if err != nil {
-		return err
-	}
-
-	fsd.conn = conn
-	return nil
-}
-
-func (fsd *Fsd) processOutgoing() {
-	for outgoing := range fsd.outgoing {
-		data := fmt.Sprintf("%s", outgoing)
-
-		if _, err := fsd.conn.Write([]byte(data)); err != nil {
-			Count("fsd.sending.failure", 1)
-			fsd.connect()
+func getCurrentVersion(client *etcd.Client, key string) *etcd.Response {
+	for {
+		if resp, err := client.Get(key, false, false); err == nil {
+			// failed to fetch first value
+			return resp
 		} else {
-			CountL("fsd.sending.sucess", 1, 0.1)
+			time.Sleep(time.Second)
 		}
 	}
+}
+func watchForUpdates(client *etcd.Client, key string, index uint64) {
+
+	for {
+		if _, err := client.Watch(key, index, false, addressConfig, nil); err != nil {
+			toSleep := 5 * time.Second
+
+			log.Debug("error watching etcd for key %v: %v", key, err)
+			log.Debug("retry in %v", toSleep)
+			time.Sleep(toSleep)
+		}
+	}
+
+}
+
+func watchConfiguration(client *etcd.Client, key string) {
+
+	resp := getCurrentVersion(client, key)
+	addressConfig <- resp
+
+	watchForUpdates(client, key, resp.EtcdIndex)
+}
+
+func connect() (err error) {
+	if address == "" {
+		return
+	}
+
+	log.Debug("fsd connects to %v", address)
+	if conn, err = net.Dial("udp", address); err != nil {
+		return
+	}
+
+	return
+}
+
+func processOutgoing() {
+	for {
+		select {
+		case outgoing := <-outgoing:
+			if !hasAddress() {
+				break
+			}
+
+			if _, err := conn.Write(outgoing); err != nil {
+				connect()
+			}
+		case response := <-addressConfig:
+			if response.Node != nil && response.Node.Value != "" {
+				address = response.Node.Value
+				connect()
+			}
+		}
+	}
+}
+
+func hasAddress() bool {
+	return address != ""
 }
 
 // To read about the different semantics check out
@@ -63,21 +112,22 @@ func Count(name string, value float64) {
 }
 
 func CountL(name string, value float64, rate float64) {
-	payload := createPayload(name, value) + "|c"
+	if rand.Float64() > rate {
+		return
+	}
 
-	suffix, err := rateCheck(rate)
+	payload, err := rateCheck(rate, createPayload(name, value, "c"))
 	if err != nil {
 		return
 	}
 
-	payload = payload + suffix
 	send(payload)
 }
 
 // Record the fuel tank is half-empty
 // fuel.level:0.5|g
 func Gauge(name string, value float64) {
-	payload := createPayload(name, value) + "|g"
+	payload := createPayload(name, value, "g")
 	send(payload)
 }
 
@@ -85,20 +135,47 @@ func Gauge(name string, value float64) {
 // request.latency:320|ms
 // Or a payload of a image
 // image.size:2.3|ms
-func Timer(name string, value float64) {
-	TimerL(name, value, 1.0)
+func Timer(name string, duration time.Duration) {
+	TimerL(name, duration, 1.0)
 }
 
-func TimerL(name string, value float64, rate float64) {
-	payload := createPayload(name, value) + "|ms"
+func TimerL(name string, duration time.Duration, rate float64) {
+	if rand.Float64() > rate {
+		return
+	}
 
-	suffix, err := rateCheck(rate)
+	HistogramL(name, float64(duration.Nanoseconds()/1000000), rate)
+}
+
+func Histogram(name string, value float64) {
+	HistogramL(name, value, 1.0)
+}
+
+func HistogramL(name string, value float64, rate float64) {
+	if rand.Float64() > rate {
+		return
+	}
+
+	payload, err := rateCheck(rate, createPayload(name, value, "ms"))
 	if err != nil {
 		return
 	}
 
-	payload = payload + suffix
 	send(payload)
+}
+
+// TimeSince records a named timer with the duration since start
+func TimeSince(name string, start time.Time) {
+	TimeSinceL(name, start, 1.0)
+}
+
+// TimeSince records a rated and named timer with the duration since start
+func TimeSinceL(name string, start time.Time, rate float64) {
+	if rand.Float64() > rate {
+		return
+	}
+
+	TimerL(name, time.Now().Sub(start), rate)
 }
 
 func Time(name string, lambda func()) {
@@ -106,45 +183,43 @@ func Time(name string, lambda func()) {
 }
 
 func TimeL(name string, rate float64, lambda func()) {
-	start := time.Now()
-	lambda()
-	TimerL(name, float64(time.Now().Sub(start).Nanoseconds()/1000000), rate)
+	if rand.Float64() > rate {
+		lambda()
+	} else {
+		start := time.Now()
+		lambda()
+		TimeSinceL(name, start, rate)
+	}
 }
 
 // Track a unique visitor id to the site.
 // users.uniques:1234|s
 func Set(name string, value float64) {
-	payload := createPayload(name, value) + "|s"
+	payload := createPayload(name, value, "s")
 	send(payload)
 }
 
-func createPayload(name string, value float64) (payload string) {
-	payload = fmt.Sprintf("%s:%f", name, value)
-	return payload
+func createPayload(name string, value float64, suffix string) string {
+	// we spend a lot of time in this code
+	return name + ":" + strconv.FormatFloat(value, 'f', -1, 64) + "|" + suffix
+	//return fmt.Sprintf("%s:%f|%s", name, value, suffix)
 }
 
-func rateCheck(rate float64) (suffix string, err error) {
+func rateCheck(rate float64, payload string) (string, error) {
 	if rate < 1 {
-		if rand.Float64() < rate {
-			return fmt.Sprintf("|@%f", rate), nil
-		}
-	} else {
-		return "", nil
+		return payload + fmt.Sprintf("|@%f", rate), nil
+	} else { // rate is 1.0 == all samples should be sent
+		return payload, nil
 	}
 
 	return "", errors.New("Out of rate limit")
 }
 
 func send(payload string) {
-	length := float64(len(Instance.outgoing))
-	capacity := float64(cap(Instance.outgoing))
-
-	Gauge("fsd.buffer.fillrate", capacity/length)
+	length := float64(len(outgoing))
+	capacity := float64(cap(outgoing))
 
 	if length < capacity*0.9 {
-		Instance.outgoing <- payload
-		CountL("fsd.buffer.success", 1, 0.1)
-	} else {
-		CountL("fsd.buffer.failure", 1, 0.1)
+		outgoing <- []byte(payload)
 	}
 }
